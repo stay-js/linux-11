2c4450ec0fef2a78cecd2864e8063297b84ac670
Zétény Nagy <nagyzeteny6@gmail.com>
2024-02-15 16:04:27 +0100

    fuggveny.sh propper discr, better formatting

--- 04/fuggveny.sh
+++ 04/fuggveny.sh
@@ -5,10 +5,10 @@ function gyok {
 }
 
 function masodfoku {
-  discriminant=$(echo "sqrt(($2^2) - (4*$1*$3))" | bc)
+  discriminant=$(echo "($2^2) - (4*$1*$3)" | bc)
 
-  x1=$(echo "((-1*$2) + $discriminant)/(2*$1)" | bc)
-  x2=$(echo "((-1*$2) - $discriminant)/(2*$1)" | bc)
+  x1=$(echo "((-1*$2) + sqrt($discriminant)) / (2*$1)" | bc)
+  x2=$(echo "((-1*$2) - sqrt($discriminant)) / (2*$1)" | bc)
 
   echo "x1=$x1, x2=$x2"
 }
