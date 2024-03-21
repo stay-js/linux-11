d0e7b59eb99fe38d49d02d03ab286a965f875596
Nagy Zétény <nagyzeteny6@gmail.com>
2024-03-21 17:24:22 +0100

    fix eq check

--- user-management/03/test.sh
+++ user-management/03/test.sh
@@ -2,7 +2,7 @@
 full_name=$(getent passwd $USER | cut -d ':' -f 5 | cut -d ',' -f1)
 last_name=$(echo $full_name | cut -d ' ' -f2)
 
-if [ $last_name = "" ]; then
+if [[ $last_name == "" ]]; then
   echo "Hello Garfield."
 else
   echo "Hello $full_name."
