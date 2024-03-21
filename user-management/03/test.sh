5dd3f749150857a1865360a93430653be5c4a450
Nagy Zétény <nagyzeteny6@gmail.com>
2024-03-21 17:19:48 +0100

    add user-management 3 feladat

---
+++ user-management/03/test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+full_name=$(getent passwd $USER | cut -d ':' -f 5 | cut -d ',' -f1)
+last_name=$(echo $full_name | cut -d ' ' -f2)
+
+if [ $last_name = "" ]; then
+  echo "Hello Garfield."
+else
+  echo "Hello $full_name."
+fi
