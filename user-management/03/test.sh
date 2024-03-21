9c89dd26e407a9929a4a9d24aebd3126c21d135c
Nagy Zétény <nagyzeteny6@gmail.com>
2024-03-21 19:37:31 +0100

    formatting

--- user-management/03/test.sh
+++ user-management/03/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-full_name=$(getent passwd $USER | cut -d ':' -f 5 | cut -d ',' -f1)
-last_name=$(echo $full_name | cut -d ' ' -f2)
+full_name=$(getent passwd $USER | cut -d ':' -f 5 | cut -d ',' -f 1)
+last_name=$(echo $full_name | cut -d ' ' -f 2)
 
 if [[ $last_name == "" ]]; then
   echo "Hello Garfield."
